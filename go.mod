aa206f1c615f7e47885bbd7fddb2a4bcbad46845
Restray <contact@restray.org>
2022-04-20 00:23:31 +0200

    Add parallele filenamename tests + loading bars + output file

--- go.mod
+++ go.mod
@@ -3,7 +3,16 @@ module github.com/restray/uniquefiles
 go 1.18
 
 require (
+	github.com/VividCortex/ewma v1.1.1 // indirect
+	github.com/briandowns/spinner v1.18.1 // indirect
+	github.com/cheggaaa/pb v1.0.29 // indirect
+	github.com/cheggaaa/pb/v3 v3.0.8 // indirect
+	github.com/fatih/color v1.10.0 // indirect
 	github.com/gookit/color v1.5.0 // indirect
+	github.com/mattn/go-colorable v0.1.8 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/mattn/go-runewidth v0.0.12 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/xo/terminfo v0.0.0-20210125001918-ca9a967f8778 // indirect
-	golang.org/x/sys v0.0.0-20210330210617-4fbd30eecc44 // indirect
+	golang.org/x/sys v0.0.0-20210403161142-5e06dd20ab57 // indirect
 )
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"crypto/sha256"
 	"flag"
 	"fmt"
@@ -9,7 +10,10 @@ import (
 	"os"
 	"path/filepath"
 	"sort"
+	"time"
 
+	"github.com/briandowns/spinner"
+	"github.com/cheggaaa/pb"
 	"github.com/gookit/color"
 )
 
@@ -25,6 +29,8 @@ type UniqueFiles struct {
 	files    map[string][]File
 	rawFiles []File
 }
+
+var NoVerboseMode bool = false
 
 func getFilesFromArgs(args []string, showError bool) []string {
 	paths := []string{}
@@ -59,6 +65,9 @@ func getFilesFromArgs(args []string, showError bool) []string {
 func getFilesInDirectory(path string) []File {
 	var files []File
 	err := filepath.WalkDir(path, func(pathWalk string, d fs.DirEntry, err error) error {
+		if os.IsPermission(err) {
+			return nil
+		}
 		if err != nil {
 			fmt.Println(err)
 			panic(err)
@@ -85,20 +94,100 @@ func remove(s []File, i int) []File {
 	return s[:len(s)-1]
 }
 
+func getFileHash(path string) string {
+	hasher := sha256.New()
+	f, err := os.Open(path)
+
+	if os.IsPermission(err) {
+		return ""
+	}
+
+	if os.IsPermission(err) {
+		fmt.Println(err)
+		panic(err)
+	}
+
+	defer f.Close()
+	if _, err := io.Copy(hasher, f); err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+
+	return fmt.Sprintf("%x", hasher.Sum(nil))
+}
+
+func loadPaths(paths []string) []File {
+	rawFiles := []File{}
+
+	s := spinner.New(spinner.CharSets[9], 100*time.Millisecond)
+
+	if !NoVerboseMode {
+		s.Start()
+		defer s.Stop()
+	}
+
+	// Get the files in the directory
+	for _, path := range paths {
+		s.Prefix = "Loading files from " + path + " "
+		rawFiles = append(rawFiles, getFilesInDirectory(path)...)
+	}
+
+	return rawFiles
+}
+
+type ByPath struct {
+	files    []File
+	filename string
+}
+
+func testDuplicateFilenames(done chan ByPath, fileToCheck []File, files []File) {
+	for _, file := range fileToCheck {
+		information := ByPath{nil, file.name}
+		information.files = []File{}
+		for _, file2 := range files {
+			if file.name == file2.name && file.path != file2.path {
+				information.files = append(information.files, file)
+			}
+		}
+		done <- information
+	}
+}
+
 func main() {
 	noVerb := flag.Bool("no-verbose", false, "Don't display verbose output and colors")
 	strict := flag.Bool("strict", false, "Only display strict equals files")
+	outputFilename := flag.String("output", "", "Only display strict equals files")
 	flag.Parse()
 
-	if !*noVerb {
+	NoVerboseMode = *noVerb
+
+	if !NoVerboseMode {
 		color.Warn.Tips("[!] Verbose mode enabled, to disable it specify add the option: -no-verbose")
 	} else {
 		color.Disable()
 	}
 
-	if *strict && !*noVerb {
+	if *strict && !NoVerboseMode {
 		color.Warn.Tips("[!] Strict mode enabled, to disable it specify remove the option: -strict")
 	}
+
+	f := os.Stdout
+
+	if *outputFilename == "" {
+		color.Warn.Tips("[!] No output file specified, printing to console. Specify -output filename to output to a file")
+	} else {
+		var err error
+		// If the file doesn't exist, create it, or append to the file
+		f, err = os.OpenFile(*outputFilename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			panic(err)
+		}
+
+		defer f.Close()
+	}
+
+	w := bufio.NewWriter(f)
 
 	// Define the global struct
 	prog := UniqueFiles{}
@@ -110,30 +199,44 @@ func main() {
 		panic("No specified path")
 	}
 
-	// Get the files in the directory
-	for _, path := range prog.path {
-		prog.rawFiles = append(prog.rawFiles, getFilesInDirectory(path)...)
-	}
+	prog.rawFiles = loadPaths(prog.path)
 
-	if !*noVerb {
+	if !NoVerboseMode {
 		fmt.Println("Duplicated files:")
 	}
 
 	// Unique entry in list
 	files := make(map[string][]File)
 
+	bar := pb.StartNew(len(prog.rawFiles))
+
+	done := make(chan ByPath)
 	// Get duplicate filenames
-	for _, file := range prog.rawFiles {
-		for _, file2 := range prog.rawFiles {
-			if file.name == file2.name && file.path != file2.path {
-				if _, exists := files[file.name]; !exists {
-					files[file.name] = []File{file}
-				} else {
-					files[file.name] = append(files[file.name], file)
-				}
-			}
+	maxThreads := 50
+	if len(prog.rawFiles) < maxThreads {
+		maxThreads = len(prog.rawFiles)
+	}
+	for i := 0; i < maxThreads; i++ {
+		min := i * (len(prog.rawFiles) / maxThreads)
+		max := i*(len(prog.rawFiles)/maxThreads) + (len(prog.rawFiles) / maxThreads)
+		if i == maxThreads {
+			go testDuplicateFilenames(done, prog.rawFiles[min:], prog.rawFiles)
+		} else {
+			go testDuplicateFilenames(done, prog.rawFiles[i:max], prog.rawFiles)
 		}
 	}
+
+	for i := 0; i < len(prog.rawFiles); i++ {
+		bar.Increment()
+
+		information := <-done
+		if _, exists := files[information.filename]; !exists {
+			files[information.filename] = information.files
+		} else {
+			files[information.filename] = append(files[information.filename], information.files...)
+		}
+	}
+	bar.Finish()
 
 	prog.files = make(map[string][]File)
 
@@ -155,19 +258,9 @@ func main() {
 	if *strict {
 		for filename, files := range prog.files {
 			if len(files) > 1 {
+
 				for i, file := range files {
-					hasher := sha256.New()
-					f, err := os.Open(file.path)
-					if err != nil {
-						fmt.Println(err)
-						panic(err)
-					}
-					defer f.Close()
-					if _, err := io.Copy(hasher, f); err != nil {
-						fmt.Println(err)
-						panic(err)
-					}
-					prog.files[filename][i].hash = fmt.Sprintf("%x", hasher.Sum(nil))
+					prog.files[filename][i].hash = getFileHash(file.path)
 				}
 
 				duplicateFiles := []File{}
@@ -188,14 +281,19 @@ func main() {
 
 	totalFiles := 0
 	totalDupFile := 0
+
+	if *outputFilename != "" {
+		color.Disable()
+	}
+
 	for filename, files := range prog.files {
 		lenFile := len(files)
 		if lenFile > 1 {
 			totalDupFile++
 			totalFiles += lenFile
 
-			if !*noVerb {
-				color.Bold.Println(filename, ":")
+			if !NoVerboseMode {
+				fmt.Fprintln(w, color.Bold.Sprint(filename, ":"))
 			}
 
 			for index, file := range files {
@@ -203,21 +301,25 @@ func main() {
 				incrSize := uint8(float64(index) / float64(lenFile-1) * 255)
 				s := color.RGB(incrSize, 255-incrSize, 0)
 
-				s.Print(file.path)
+				fmt.Fprintf(w, s.Sprint(file.path))
 
-				if !*noVerb {
-					s.Printf(" (%db) - Modified on: %s", file.info.Size(), file.info.ModTime())
+				if !NoVerboseMode {
+					fmt.Fprintf(w, s.Sprintf(" (%db) - Modified on: %s", file.info.Size(), file.info.ModTime()))
 					if *strict {
-						s.Printf(" - Hash: %s", file.hash)
+						fmt.Fprintf(w, s.Sprintf(" - Hash: %s", file.hash))
 					}
 				}
-				s.Println()
+				fmt.Fprintln(w, s.Sprint())
 			}
-			fmt.Println()
+			fmt.Fprintln(w, fmt.Sprint())
 		}
 	}
 
-	if !*noVerb {
+	if *outputFilename != "" {
+		color.ResetOptions()
+	}
+
+	if !NoVerboseMode {
 		color.Bold.Printf("Total files: %d (with %d uniques)\n", totalFiles, totalDupFile)
 	}
 }
